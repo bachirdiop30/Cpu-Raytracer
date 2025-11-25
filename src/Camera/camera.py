930884f2acb74a7c5178a1e2ba0a34c3607eb891
Yanis Am <yanis.amedjkane@epitech.eu>
2025-11-25 21:52:20 +0100

    feat: multiprocessing for handling scan lines

--- src/Camera/camera.py
+++ src/Camera/camera.py
@@ -5,6 +5,9 @@ from Core.hittable import *
 from Core.ray import Ray
 from Display.PPMWriter import PPMWriter
 import random
+import concurrent.futures
+import functools
+import multiprocessing
 
 class Camera:
     def __init__(self, img_width, aspect_ratio, samples_per_pixel, max_ray_bounces) -> None:
@@ -34,11 +37,8 @@ class Camera:
         if self._max_ray_bounces <= 0: 
             return Color3(0, 0, 0)
 
-        t = hit_sphere(Vector3(0,0,-1), 0.5, ray)
         rec = HitRecord()
         if world.hit(ray, 0.001, float('inf'), rec):
-            point = ray.at(t)
-            normal = (point - Vector3(0, 0, -1)).normalize()
             vec_direction = random_on_hemisphere(rec.normal)
             new_ray = Ray(rec.point, vec_direction)
             return 0.5 * self.ray_color(new_ray, world, max_bounce_count - 1)
@@ -46,6 +46,17 @@ class Camera:
         unit_dir = ray.direction.normalize()
         a = 0.5 * (unit_dir.y + 1.0)
         return (1.0 - a) * Color3(1,1,1) + a * Color3(0.5, 0.7, 1.0)
+
+    def process_scanlines(self, j, world):
+        row_colors = []
+        for i in range(self._img_width):
+            pixel_color = Color3(0, 0, 0)
+            for sample in range(self._samples_per_pixel):
+                ray = self.get_ray(i, j)
+                pixel_color += self.ray_color(ray, world, self._max_ray_bounces)
+            final_color = pixel_color * self._pixel_sample_scale
+            row_colors.append(final_color)
+        return row_colors
 
     def initialize_camera(self):
         self._img_height = int(self._img_width / self._aspect_ratio)
@@ -75,32 +86,11 @@ class Camera:
     def render(self, world):
         #TODO: add hittables abstraction layer t primitives
         self.initialize_camera()
-        
-        for j in range(self._img_height):
-            row = []
-            for i in range(self._img_width):
-                color = Color3(0, 0, 0)
-                for sample in range(self._samples_per_pixel):
-                    ray = self.get_ray(i, j)
-                    color += self.ray_color(ray, world, self._max_ray_bounces)
-                row.append(color * self._pixel_sample_scale)
-            self._img_ro_render.append(row)
-
-        # for j in range(self._img_height):
-        #     row = []
-        #     for i in range(self._img_width):
-        #         color = Color3(0, 0, 0)
-        #         for sample in range(self._samples_per_pixel):
-        #             ray = self.get_ray(i, j)
-        #             color += self.ray_color(ray, self._max_ray_bounces)
-        #             # print(f"Sampling : {sample}")
-
-        #         #pixel_center = self._pixel00_loc + (i+0.5)*self._pixel_delta_u + (j+0.5)*self._pixel_delta_v
-        #         #ray_direction = pixel_center - self._center
-        #         #r = Ray(self._center, ray_direction)
-        #         row.append(color * self._pixel_sample_scale)
-        #     self._img_ro_render.append(row)
-
+        num_cores = multiprocessing.cpu_count()
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_cores) as executor:
+            func = functools.partial(self.process_scanlines, world=world)
+            results = executor.map(func, range(self._img_height))
+            self._img_ro_render = list(results)
         self.write_img_to_file()
 
 
